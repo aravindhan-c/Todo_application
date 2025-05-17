aa8916af9e8f3c53d4854dbede3592361b9f4180
aravindhan <aravindhan.changer@gmail.com>
2025-05-17 19:41:18 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    Ankit's ➡️ TODO LIST
+                    Aravindhan's ➡️ TODO LIST
                 </Row>
   
                 <hr />
